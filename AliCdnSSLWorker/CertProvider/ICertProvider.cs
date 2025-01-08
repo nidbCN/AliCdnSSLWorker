a76119c359f03e19b0e337edd1d175eb3071321f
Gaein nidb <mail@gaein.cn>
2025-01-08 12:31:21 +0800

    feat: add cert provider, impl local provider

---
+++ AliCdnSSLWorker/CertProvider/ICertProvider.cs
@@ -0,0 +1,21 @@
+﻿using AliCdnSSLWorker.Models;
+
+namespace AliCdnSSLWorker.CertProvider;
+
+public interface ICertProvider
+{
+    public Task<CertInfo?> GetMatchedCertByDomain(DomainInfo domain)
+        => GetMatchedCertByDomain(domain, CancellationToken.None);
+
+    public Task<CertInfo?> GetMatchedCertByDomain(DomainInfo domain, CancellationToken token);
+
+    public Task<CertInfo?> GetMatchedCertByDomain(string identify)
+        => GetMatchedCertByDomain(identify, CancellationToken.None);
+
+    public Task<CertInfo?> GetMatchedCertByDomain(string identify, CancellationToken token);
+
+    public Task<IList<CertInfo>> GetAllCerts()
+        => GetAllCerts(CancellationToken.None);
+
+    public Task<IList<CertInfo>> GetAllCerts(CancellationToken token);
+}
---
+++ AliCdnSSLWorker/Configs/LocalProviderConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace AliCdnSSLWorker.Configs;
+
+public record LocalCertProviderConfig
+{
+    public required string SearchPath { get; set; }
+    public required bool RecursionSearch { get; set; }
+    public required string CertFileName { get; set; } = "fullchain.pem";
+    public required string PrivateKeyFileName { get; set; } = "privkey.pem";
+}
