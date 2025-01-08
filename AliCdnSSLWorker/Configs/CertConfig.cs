c163799ad01265ef9a9b51c396de42de84c0eb2c
Gaein nidb <mail@gaein.cn>
2025-01-08 13:15:02 +0800

    refactor(certService): remove function add, impl function scan

--- AliCdnSSLWorker/Configs/CertConfig.cs
+++ AliCdnSSLWorker/Configs/CertConfig.cs
@@ -2,14 +2,6 @@
 
 public record CertConfig
 {
-    public required string CertSearchPath { get; init; }
-
-    // ReSharper disable once StringLiteralTypo
-    public string CertFileName { get; set; } = "fullchain.pem";
-    // ReSharper disable once StringLiteralTypo
-    public string PrivateKeyFileName { get; set; } = "privkey.pem";
-
-    public bool RecursionSearch { get; init; } = true;
     public uint CacheTimeoutMin { get; init; } = 30;
 
     public HashSet<string> DomainList { get; init; } = [];
