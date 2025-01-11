2811b9ddb271285883f2e87043f8986b415e3040
Gaein nidb <mail@gaein.cn>
2025-01-11 21:26:59 +0800

    refactor: move TryUploadAllCert to AliCdnService

--- AliCdnSSLWorker/Monitors/TimerMonitor.cs
+++ AliCdnSSLWorker/Monitors/TimerMonitor.cs
@@ -4,48 +4,35 @@ using Microsoft.Extensions.Options;
 
 namespace AliCdnSSLWorker.Monitors;
 
-public class TimerMonitor(ILogger<TimerMonitor> logger,
+public class TimerMonitor(
+    ILogger<TimerMonitor> logger,
     IOptions<TimerMonitorConfig> monitorOptions,
-    AliCdnService aliCdnService,
-    CertService certService) : BackgroundService
+    AliCdnService aliCdnService
+    ) : BackgroundService
 {
-    public bool TryUpdate()
+    private bool TryUpdate()
     {
-        if (!aliCdnService.TryGetRemoteCerts(out var infos))
-        {
-            logger.LogError("Can not get remote cert infos from CDN.");
-            return false;
-        }
-
         // select certs expire before next update.
         var now = DateTime.Now;
-
-        foreach (var remoteCert in infos!)
-        {
-            if (remoteCert.CertExpireDate - now > monitorOptions.Value.RefreshInterval)
-                continue;
-
-            logger.LogInformation("Remote cert `{cn}` will expire at {t:c}. Upload local cert.", remoteCert.CertCommonName, remoteCert.CertExpireDate);
-
-            if (certService.TryGetCertByDomain(remoteCert.CertCommonName, out var localCert))
-            {
-                if (aliCdnService.TryUploadCert(remoteCert.CertCommonName.OriginString, localCert!))
-                    logger.LogInformation("Success upload cert for {d}.", remoteCert.CertCommonName);
-            }
-            else
-            {
-                logger.LogWarning("Can not found cert for `{cn}`, skip.", remoteCert.CertCommonName);
-            }
-        }
-
-        return true;
+        var interval = monitorOptions.Value.RefreshInterval;
+        return aliCdnService.TryUploadAllCert(r => r.CertExpireDate - interval > now);
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            TryUpdate();
+            try
+            {
+                if (!TryUpdate())
+                {
+                    logger.LogWarning("Upload failed.");
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Upload crashed.");
+            }
 
             await Task.Delay(monitorOptions.Value.RefreshInterval, stoppingToken);
         }
