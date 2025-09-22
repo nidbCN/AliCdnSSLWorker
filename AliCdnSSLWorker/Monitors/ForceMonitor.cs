1d25a3e73902ff6f376fadb8bb1917cbdec1da9a
Dishonek <Dishonek@Outlook.com>
2025-09-22 11:37:52 +0800

    feat: add /certs/ HTTP endpoint for certificate listing

--- AliCdnSSLWorker/Monitors/ForceMonitor.cs
+++ AliCdnSSLWorker/Monitors/ForceMonitor.cs
@@ -1,4 +1,6 @@
 ﻿using System.Net;
+using System.Text;
+using System.Text.Json;
 using AliCdnSSLWorker.Configs;
 using AliCdnSSLWorker.Services;
 using Microsoft.Extensions.Options;
@@ -14,6 +16,70 @@ public class ForceMonitor(
     public bool TryUpdateAll()
         => aliCdnService.TryUploadAllCert(r => true);
 
+    private JsonSerializerOptions _jso = new JsonSerializerOptions
+    {
+        WriteIndented = true,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+    };
+
+    private void GetNotFoundResponse(HttpListenerResponse resp)
+    {
+        resp.StatusCode = (int)HttpStatusCode.NotFound;
+    }
+    private void GetDefaultResponse(HttpListenerResponse resp)
+    {
+        string responseString = "HTTP Service is running successfully!";
+        byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+
+        resp.ContentType = "text/plain";
+        resp.ContentEncoding = Encoding.UTF8;
+        resp.ContentLength64 = buffer.Length;
+        resp.StatusCode = (int)HttpStatusCode.OK;
+
+        resp.OutputStream.Write(buffer, 0, buffer.Length);
+    }
+    private void GetForceRefreshResponse(HttpListenerResponse resp)
+    {
+        try
+        {
+            TryUpdateAll();
+            resp.StatusCode = (int)HttpStatusCode.OK;
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "An error occured during update all certs.");
+            resp.StatusCode = (int)HttpStatusCode.InternalServerError;
+        }
+    }
+
+    private void GetCertsResponse(HttpListenerResponse resp)
+    {
+        try
+        {
+            if (!aliCdnService.TryGetRemoteCerts(out var infos))
+            {
+                resp.StatusCode = (int)HttpStatusCode.InternalServerError;
+                resp.StatusDescription = "Certificates not found";
+                return;
+            }
+            var json = JsonSerializer.Serialize(infos, _jso);
+            resp.ContentType = "application/json";
+            resp.ContentEncoding = Encoding.UTF8;
+            resp.StatusCode = (int)HttpStatusCode.OK;
+
+            using (var writer = new StreamWriter(resp.OutputStream, Encoding.UTF8))
+            {
+                writer.Write(json);
+            }
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "Error retrieving certificates");
+            resp.StatusCode = (int)HttpStatusCode.InternalServerError;
+            resp.StatusDescription = "Internal Server Error";
+        }
+    }
+
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
         return Task.Run(() =>
@@ -27,7 +93,8 @@ public class ForceMonitor(
              if (monitorOptions.Value.GetIpAddress().Equals(IPAddress.Any))
                  ip = "+";
 
-             listener.Prefixes.Add($"http://{ip}:{port}/force_refresh/");
+             listener.Prefixes.Add($"http://{ip}:{port}/");
+
              try
              {
                  listener.Start();
@@ -56,16 +123,20 @@ public class ForceMonitor(
              {
                  var ctx = listener.GetContext();
                  using var resp = ctx.Response;
-
-                 try
+                 switch (ctx.Request.RawUrl)
                  {
-                     TryUpdateAll();
-                     resp.StatusCode = (int)HttpStatusCode.OK;
-                 }
-                 catch (Exception e)
-                 {
-                     logger.LogError(e, "An error occured during update all certs.");
-                     resp.StatusCode = (int)HttpStatusCode.InternalServerError;
+                     case "/certs":
+                         GetCertsResponse(resp);
+                         break;
+                     case "/force_refresh":
+                         GetForceRefreshResponse(resp);
+                         break;
+                     case "/":
+                         GetDefaultResponse(resp);
+                         break;
+                     default:
+                         GetNotFoundResponse(resp);
+                         break;
                  }
 
                  resp.Close();
